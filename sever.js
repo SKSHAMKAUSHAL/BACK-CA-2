95fbc2addb2915ff1ee6b27167d1e43390e78ab9
Sksham Kaushal <skshamkaushal@gmail.com>
2025-03-04 10:11:36 +0530

    setup

--- sever.js
+++ sever.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const LoginPage = require('./schema');
+const {user}=require('./model/user')
 
 const app = express();
 app.use(express.json()); 
