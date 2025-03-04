1a17963dffa98c14bf01c50431d1cafc6ace691c
Sksham Kaushal <skshamkaushal@gmail.com>
2025-03-04 10:06:53 +0530

    setup

--- sever.js
+++ sever.js
@@ -1,25 +1,42 @@
-require('dotenv').config();
-const express = require ('express');
+const express = require('express');
+const mongoose = require('mongoose');
+const LoginPage = require('./schema');
 
-const Port=process.env.PORT||8080;
+const app = express();
+app.use(express.json()); 
 
-const app =express();
+const PORT = 8080;
 
-app.use(express.json())
-
-const {user}=require('./model/user')
-
-
-app.get('/',(req,res)=>{
-    res.send("HEY THIS IS MY CA 2 OF BACKEND");
+mongoose.connect("mongodb://127.0.0.1:27017/mydatabase", { 
+    useNewUrlParser: true, 
+    useUnifiedTopology: true 
 })
+.then(() => console.log("MongoDB Connected"))
+.catch(err => console.log(err));
 
+app.post('/login', async (req, res) => {
+    const { email, password } = req.body;
 
-// app.post('/',(req,res)=>{
-    
-//     res.send("HEY THIS IS MY CA 2 OF BACKEND");
-// })
+    if (!email) {
+        return res.status(400).json({ error: "Email required" });
+    } 
+    if (!password) {
+        return res.status(400).json({ error: "Password required" });
+    }
 
-app.listen(Port,(req,res)=>{
-    console.log(`this server is running at http://localhost:${Port}`);
-})
+    try {
+        
+        const user = await LoginPage.findOne({ email, password });
+        if (user) {
+            return res.status(200).json({ message: "Successfully logged in!" });
+        } else {
+            return res.status(401).json({ error: "Invalid credentials" });
+        }
+    } catch (err) {
+        return res.status(500).json({ error: "Internal Server Error" });
+    }
+});
+
+app.listen(PORT, () => {
+    console.log(`App is running at http://localhost:${PORT}`);
+});
